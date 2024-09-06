ef6904b3a8826be2b02d7d0cc91da24fafa1ecd9
Dmitry Inke <angranity1998@gmail.com>
2024-09-06 23:02:29 +0300

    feat(users): add user creation with password hashing and retrieval via GraphQL using Prisma and bcryptjs

--- apps/distributed-job-manager-auth/src/app/app.module.ts
+++ apps/distributed-job-manager-auth/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -11,6 +12,8 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
       driver: ApolloDriver,
       autoSchemaFile: true,
     }),
+    UsersModule,
   ],
+  providers: [],
 })
 export class AppModule {}
