72782bb195f1438c3d4dbafb001720055ea7b501
Dmitry Inke <angranity1998@gmail.com>
2024-10-28 20:03:39 +0200

    Created User Verification, Sign JWT cookie, extend tests

--- apps/distributed-job-manager-auth/src/app/users/users.module.ts
+++ apps/distributed-job-manager-auth/src/app/users/users.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
+import { UsersResolver } from './users.resolver';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-    imports: [PrismaModule],
-    providers: [UsersResolver, UsersService],
-    exports: [UsersService],
+  imports: [PrismaModule],
+  providers: [UsersResolver, UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
