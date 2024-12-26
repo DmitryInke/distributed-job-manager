cd6ea7902d82948f423b97d111376c543922a66d
Dmitry Inke <angranity1998@gmail.com>
2024-12-26 09:25:03 +0200

    add pino logger, add log for graphql req/res

--- apps/jobs/src/app/app.module.ts
+++ apps/jobs/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { ConfigModule } from '@nestjs/config';
 import { JobsModule } from './jobs.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { LoggerModule } from '@distributed-job-manager/nestjs';
+import { GqlLoggingPlugin } from '@distributed-job-manager/graphql';
 
 @Module({
   imports: [
+    LoggerModule,
     ConfigModule.forRoot({ isGlobal: true }),
     JobsModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
+      plugins: [new GqlLoggingPlugin()],
       playground: {
         settings: {
           'request.credentials': 'include',
