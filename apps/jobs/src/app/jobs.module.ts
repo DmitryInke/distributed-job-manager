2b86a5d5a32edab6ef41f0a539b66e180a775cf7
Dmitry Inke <angranity1998@gmail.com>
2025-01-04 18:59:53 +0200

    prisma migrate in deployment.yaml, in jobs module provide url

--- apps/jobs/src/app/jobs.module.ts
+++ apps/jobs/src/app/jobs.module.ts
@@ -7,17 +7,22 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AUTH_PACKAGE_NAME } from '@distributed-job-manager/grpc';
 import { join } from 'path';
 import { PulsarModule } from '@distributed-job-manager/pulsar';
+import { ConfigService } from '@nestjs/config';
 @Module({
   imports: [
     DiscoveryModule,
-    ClientsModule.register([
+    ClientsModule.registerAsync([
       {
         name: AUTH_PACKAGE_NAME,
-        transport: Transport.GRPC,
-        options: {
-          package: AUTH_PACKAGE_NAME,
-          protoPath: join(__dirname, '../../libs/grpc/proto/auth.proto'),
-        },
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.GRPC,
+          options: {
+            url: configService.getOrThrow('AUTH_GRPC_SERVICE_URL'),
+            package: AUTH_PACKAGE_NAME,
+            protoPath: join(__dirname, '../../libs/grpc/proto/auth.proto'),
+          },
+        }),
+        inject: [ConfigService],
       },
     ]),
     PulsarModule,
